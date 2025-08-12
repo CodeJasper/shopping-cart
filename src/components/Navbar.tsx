3f761de46c59e08d75b4c71988febc6c42ff15e3
CodeJasper <jssv0116@hotmail.com>
2025-08-12 10:34:21 -0500

    feat: add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-lg font-bold">Shopping Cart</span>
+        <div>
+          <a href="/cart" className="mr-4">Cart</a>
+          <a href="/">Products</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
